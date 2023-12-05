9bb73c0fe1649d7ccde2a0be81045d58bc8e6354
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-12-05 09:16:13 -0300

    Add files via upload

---
+++ KittysBakery/src/main/java/com/mycompany/kittysbakery/KittysBakery.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kittysbakery;
+
+/**
+ *
+ * @author jujuf
+ */
+public class KittysBakery {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
